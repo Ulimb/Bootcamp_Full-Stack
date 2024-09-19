c584d1de828c5a0fcb36baa6e00389c15bbe96f0
Ulimb <ulisesmijaelben02@gmail.com>
2024-09-19 15:16:53 -0300

    Agrego el componente Footer, solo funciona en Home

--- Bootcamp_Full-Stack/semana-6/proyecto/Componentes/NavBarS.js
+++ Bootcamp_Full-Stack/semana-6/proyecto/Componentes/NavBarS.js
@@ -1,7 +1,29 @@
-const rutaNavBar = '../../Componentes/NavBar.html';
+// const rutaNavBar = '../../Componentes/NavBar.html';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     cargarComponente('navbar-reutilizable', rutaNavBar);
+// });
+
+// function cargarComponente(elementId, ruta) {
+//     const elemento = document.getElementById(elementId);
+//     if (elemento) {
+//         fetch(ruta)
+//             .then(response => response.text())
+//             .then(template => {
+//                 elemento.innerHTML = template;
+//                 console.log(template);
+                
+//             })
+//             .catch(error => console.error('Error al cargar el navbar:', error));
+//     }
+// }
+
+const rutaNavBar = '../../Componentes/NavBar.html';  // Ruta del navbar
+const rutaFooter = '../../Componentes/Footer.html'; // Ruta del footer
 
 document.addEventListener('DOMContentLoaded', () => {
-    cargarComponente('navbar-reutilizable', rutaNavBar);
+    cargarComponente('navbar-reutilizable', rutaNavBar); // Cargar navbar
+    cargarComponente('footer-reutilizable', rutaFooter); // Cargar footer
 });
 
 function cargarComponente(elementId, ruta) {
@@ -11,9 +33,8 @@ function cargarComponente(elementId, ruta) {
             .then(response => response.text())
             .then(template => {
                 elemento.innerHTML = template;
-                console.log(template);
-                
+                console.log(`Componente ${elementId} cargado`);
             })
-            .catch(error => console.error('Error al cargar el navbar:', error));
+            .catch(error => console.error(`Error al cargar el componente ${elementId}:`, error));
     }
 }
