3308a59d70cdc548133fa135f54df0254996e639
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2024-09-20 22:53:53 -0300

    Todos los productos aregados

---
+++ Bootcamp_Full-Stack/semana-6/proyecto/Pages/Productos/CargaProductos.js
@@ -0,0 +1,190 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const productosContainer = document.getElementById('productos-container');
+    const categoria = window.location.pathname.split('/').pop().split('.')[0];
+    const productos = productosPorCategoria[categoria]; // obtiene productos por categoría
+    let cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+    if (!productos) {
+        productosContainer.innerHTML = `<p>No se encontraron productos para esta categoría.</p>`;
+        return;
+    }
+
+    productosContainer.innerHTML = '';
+
+    productos.forEach((producto, index) => {
+        const precioConDescuento = producto.precio - (producto.precio * (producto.descuento / 100));
+        const productoEnCarrito = cart.find(item => item.nombre === producto.nombre);
+        const cantidadEnCarrito = productoEnCarrito ? productoEnCarrito.cantidad : 0;
+
+        const precioHTML = producto.descuento > 0 ? `
+            <p class="price-original text-decoration-line-through">$${producto.precio.toFixed(2)}</p>
+        ` : `
+            <p class="fw-bold">$${producto.precio.toFixed(2)}</p>
+        `;
+
+        const imgProductoOferta = producto.descuento > 0 ? `
+            <div class="col-12 col-sm-12 col-md-3 bg-secondary p-4 position-relative">
+                <img src="${producto.imagen}" class="img-fluid rounded-5 img-producto" alt="Imagen de ${producto.nombre}">
+                <h4 class="Text-Ofertas">PRODUCTO EN OFERTA</h4>
+            </div>
+        ` : `
+            <div class="col-12 col-sm-12 col-md-3 bg-secondary p-4">
+                <img src="${producto.imagen}" class="img-fluid rounded-5" alt="Imagen de ${producto.nombre}">
+            </div>
+        `;
+
+        const descuentoHTML = producto.descuento > 0 ? `
+            <div class="row d-flex align-items-center h-25 p-2">
+                <div class="col-6 position-relative d-flex"> 
+                    <img src="../../imagenes/iconos/oferta.png" class="img-ofertas" alt="Icono oferta">
+                    <p class="price-ofertas">${producto.descuento}%</p>
+                </div>
+                <div class="col-6" id="precio-producto-${index}">
+                    <p class="fw-bold text-danger mb-0">$${precioConDescuento.toFixed(2)}</p>
+                </div>
+            </div>
+        ` : '';
+
+        const cartButtonHTML = cantidadEnCarrito > 0 ? `
+            <div class="col-12 d-flex justify-content-center">
+                <div class="bg-primary quantity-container d-flex align-items-center justify-content-between p-2 w-60">
+                    <p class="text-white mb-0">Unidades</p>
+                    <div class="d-flex align-items-center">
+                        <button class="btn-decrease" data-index="${index}">-</button>
+                        <p class="mb-0 units bg-white mx-3 p-2 rounded" id="units-${index}">${cantidadEnCarrito}</p>
+                        <button class="btn-increase" data-index="${index}">+</button>
+                    </div>
+                </div>
+            </div>
+        ` : `
+            <div class="col-12">
+                <button class="bg-primary w-100 d-flex align-items-center justify-content-start btn-add-cart" data-index="${index}">
+                    <img src="../../imagenes/iconos/shopping.png" width="25px" height="25px" class="me-2">
+                    <p class="mb-0 flex-grow-1 text-center text-white">Agregar al Carrito</p>
+                </button>
+            </div>
+        `;
+
+        const productoHTML = `
+            <div class="container mb-5" id="producto-${index}">
+                <div class="row">
+                    <div class="col-12 bg-primary rounded-custom-h2">
+                        <div class="text-white p-3 text-center">
+                            <h2 class="m-0">${producto.nombre}</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="row border border-1 border-secondary">
+                    ${imgProductoOferta}
+                    <div class="col-12 col-sm-12 col-md-4 d-flex align-items-center justify-content-center border-custom border-1 border-secondary">
+                        <div class="p-5 text-center">
+                            ${producto.descripcion.map(linea => `<p class="mb-1">${linea}</p>`).join('')}
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-5">
+                        <div class="row d-flex align-items-center h-25 p-2">
+                            <div class="col-6">
+                                <p class="fw-bold">Precio x KG</p>
+                            </div>
+                            <div class="col-6" id="precio-producto-${index}">
+                                ${precioHTML}
+                            </div>
+                        </div>
+                        ${descuentoHTML}
+                        <div class="row d-flex align-items-center h-25 p-2" id="cart-btn-${index}">
+                            ${cartButtonHTML}
+                        </div>
+                        <div class="row d-flex align-items-center h-25 p-2">
+                            <div class="col-6">
+                                <p class="fw-bold">Subtotal:</p>
+                            </div>
+                            <div class="col-6">
+                                <p class="fw-bold" id="subtotal-${index}">$${(precioConDescuento * (cantidadEnCarrito || 1)).toFixed(2)}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        productosContainer.innerHTML += productoHTML;
+    });
+
+    const saveCartToLocalStorage = () => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    };
+  
+
+    const addCartEventListeners = () => {
+        const decreaseButtons = document.querySelectorAll('.btn-decrease');
+        const increaseButtons = document.querySelectorAll('.btn-increase');
+        const addCartButtons = document.querySelectorAll('.btn-add-cart');
+
+        decreaseButtons.forEach(button => {
+            button.removeEventListener('click', handleDecrease); 
+            button.addEventListener('click', handleDecrease);
+        });
+
+        increaseButtons.forEach(button => {
+            button.removeEventListener('click', handleIncrease); 
+            button.addEventListener('click', handleIncrease);
+        });
+
+        addCartButtons.forEach(button => {
+            button.removeEventListener('click', handleAddCart); 
+            button.addEventListener('click', handleAddCart);
+        });
+    };
+
+    const handleAddCart = (e) => {
+        const index = e.target.closest('.btn-add-cart').dataset.index;
+        const cartBtnContainer = document.getElementById(`cart-btn-${index}`);
+        const precioConDescuento = productos[index].precio - (productos[index].precio * (productos[index].descuento / 100));
+
+        cartBtnContainer.innerHTML = `
+            <div class="col-12 d-flex justify-content-center">
+                <div class="bg-primary quantity-container d-flex align-items-center justify-content-between p-2 w-60">
+                    <p class="text-white mb-0">Unidades</p>
+                    <div class="d-flex align-items-center">
+                        <button class="btn-decrease" data-index="${index}">-</button>
+                        <p class="mb-0 units bg-white mx-3 p-2 rounded" id="units-${index}">1</p>
+                        <button class="btn-increase" data-index="${index}">+</button>
+                    </div>
+                </div>
+            </div>
+        `;
+        
+        cart.push({ ...productos[index], cantidad: 1 });
+        saveCartToLocalStorage();
+        addCartEventListeners(); // Vuelve a añadir event listeners
+    };
+    const handleDecrease = (e) => {
+        const index = e.target.dataset.index;
+        const unitsElement = document.getElementById(`units-${index}`);
+        let currentUnits = parseInt(unitsElement.innerText);
+    
+        if (currentUnits > 1) {
+            currentUnits -= 1;
+            unitsElement.innerText = currentUnits;
+            const precioConDescuento = productos[index].precio - (productos[index].precio * (productos[index].descuento / 100));
+            document.getElementById(`subtotal-${index}`).innerText = `$${(precioConDescuento * currentUnits).toFixed(2)}`;
+            cart.find(item => item.nombre === productos[index].nombre).cantidad = currentUnits;
+        } else {
+            cart = cart.filter(item => item.nombre !== productos[index].nombre);
+            unitsElement.innerText = '0'; 
+        }
+        saveCartToLocalStorage(); 
+    };
+    const handleIncrease = (e) => {
+        const index = e.target.dataset.index;
+        const unitsElement = document.getElementById(`units-${index}`);
+        let currentUnits = parseInt(unitsElement.innerText) + 1;
+        unitsElement.innerText = currentUnits;
+        const precioConDescuento = productos[index].precio - (productos[index].precio * (productos[index].descuento / 100));
+        document.getElementById(`subtotal-${index}`).innerText = `$${(precioConDescuento * currentUnits).toFixed(2)}`;
+        cart.find(item => item.nombre === productos[index].nombre).cantidad = currentUnits;
+        saveCartToLocalStorage();
+    };
+
+    addCartEventListeners(); 
+});
---
+++ Bootcamp_Full-Stack/semana-6/proyecto/Pages/Productos/Productos.js
@@ -0,0 +1,196 @@
+// productos.js
+
+const productosPorCategoria = {
+    Carnes: [
+        {
+            nombre: 'Chorizo parrillero x KG',
+            descripcion: [
+                'Chorizo colorado de ternera.',
+                'El peso puede variar ligeramente.'
+            ],
+            precio: 5000,
+            imagen: '../../imagenes/categorías/carnes/chorizos.jpg',
+            descuento: 25
+        },
+        {
+            nombre: 'Lomo x KG',
+            descripcion: [
+                'Lomo de ternera.',
+                'El peso puede variar ligeramente.'
+            ],
+            precio: 8500,
+            imagen: '../../imagenes/categorías/carnes/lomo.jpg',
+            descuento: 0
+        },
+        {
+            nombre: 'Milanesas de pollo x KG',
+            descripcion: [
+                'Milanesas de carne de pechuga de pollo.',
+                'El peso puede variar ligeramente.'
+            ],
+            precio: 6000,
+            imagen: '../../imagenes/categorías/carnes/milanesas.jpg',
+            descuento: 0
+        }, 
+        {
+            nombre: 'Carne picada común x KG',
+            descripcion: [
+                'Carne picada con alto contenido de grasa.',
+                'El peso puede variar ligeramente.'
+            ],
+            precio: 4000,
+            imagen: '../../imagenes/categorías/carnes/picada.jpg',
+            descuento: 0
+        }
+    ],
+    Gaseosas: [
+        {
+            nombre: 'Coca Cola',
+            descripcion: ['Gaseosa con azucar.', 'Botella de 1 litro.'],
+            precio: 2300,
+            imagen: '../../imagenes/categorías/gaseosas/cocacola.jpg',
+            descuento: 10
+        },
+        {
+            nombre: 'SevenUp',
+            descripcion: ['Gaseosa con azucar.', 'Botella de 1 litro.'],
+            precio: 2300,
+            imagen: '../../imagenes/categorías/gaseosas/7up.jpg',
+            descuento: 0
+        },
+        {
+            nombre: 'Fanta',
+            descripcion: ['Gaseosa con azucar.', 'Botella de 1 litro.'],
+            precio: 2000,
+            imagen: '../../imagenes/categorías/gaseosas/fanta.jpg',
+            descuento: 0
+        },
+        {
+            nombre: 'Pomelo',
+            descripcion: ['Cunnington Clasia.', 'Botella de 1 litro.'],
+            precio: 1800,
+            imagen: '../../imagenes/categorías/gaseosas/pomelo.jpg',
+            descuento: 0
+        }
+
+    ], Lacteos: [
+        {
+            nombre: 'Crema de Leche',
+            descripcion: ['La Paulina', 'Contiene 350cc'],
+            precio: 1500,
+            imagen: '../../imagenes/categorías/lacteos/crema.jpg',
+            descuento: 0
+        },
+        {
+            nombre: 'Leche',
+            descripcion: ['Leche Fresca.', 'Sache de 1 litro.'],
+            precio: 1300,
+            imagen: '../../imagenes/categorías/lacteos/leche.jpg',
+            descuento: 0
+        },
+        {
+            nombre: 'Manteca',
+            descripcion: ['Manteca SarCor', 'Contiene 200g'],
+            precio: 3000,
+            imagen: '../../imagenes/categorías/lacteos/manteca.jpg',
+            descuento: 20
+        },
+        {
+            nombre: 'Quesardo',
+            descripcion: ['EL QUESO', 'El mejor queso del barrio'],
+            precio: 1000,
+            imagen: '../../imagenes/categorías/lacteos/quesosardo.jpg',
+            descuento: 0
+        }
+    ], Limpieza: [
+        {
+            nombre: 'Desinfectante',
+            descripcion: ['Desinfectante Poett', 'Botella de 900ml'],
+            precio: 2500,
+            imagen: '../../imagenes/categorías/limpieza/desinfectante.jpg',
+            descuento: 30
+        },
+        {
+            nombre: 'Detergente',
+            descripcion: ['Detergente Cif', 'Botella de 500ml.'],
+            precio: 2300,
+            imagen: '../../imagenes/categorías/limpieza/detergente.jpg',
+            descuento: 0
+        },
+        {
+            nombre: 'Esponja',
+            descripcion: ['Esponja Clasica', 'Sirve como ¡¡¡ESPONJA!!!'],
+            precio: 3000,
+            imagen: '../../imagenes/categorías/limpieza/esponja.jpg',
+            descuento: 0
+        },
+        {
+            nombre: 'Lavandina',
+            descripcion: ['Agua Lavandina comun ', 'Botella de 1 litro'],
+            precio: 1000,
+            imagen: '../../imagenes/categorías/limpieza/lavandina.jpg',
+            descuento: 0
+        }
+    ],Verduleria: [
+        {
+            nombre: 'Banana x KG',
+            descripcion: ['Banana 🐒', 'El peso puede variar ligeramente'],
+            precio: 1200,
+            imagen: '../../imagenes/categorías/verduleria/banana.jpg',
+            descuento: 0
+        },
+        {
+            nombre: 'Cebolla x KG',
+            descripcion: ['Cebolla Comun 🧅', 'El peso puede variar ligeramente.'],
+            precio: 1500,
+            imagen: '../../imagenes/categorías/verduleria/cebolla.jpg',
+            descuento: 25
+        },
+        {
+            nombre: 'Manzana x KG',
+            descripcion: ['Manzana roja 🍎', 'El peso puede variar ligeramente.'],
+            precio: 1000,
+            imagen: '../../imagenes/categorías/verduleria/manzana.jpg',
+            descuento: 0
+        },
+        {
+            nombre: 'Papa x Kg',
+            descripcion: ['Papa comun 🥔', 'El peso puede variar ligeramente'],
+            precio: 1000,
+            imagen: '../../imagenes/categorías/verduleria/papa.jpg',
+            descuento: 0
+        }
+    ],
+    Vinos: [
+        {
+            nombre: 'Helechos',
+            descripcion: ['Los Helechos', 'Soy una planta 🌿'],
+            precio: 1200,
+            imagen: '../../imagenes/categorías/vinos/helechos.jpg',
+            descuento: 0
+        },
+        {
+            nombre: 'Magnolia',
+            descripcion: ['Vino tinto Cabernet Sauvignon.', 'Reserva 2021. Origen Mendoza'],
+            precio: 6000,
+            imagen: '../../imagenes/categorías/vinos/magnolia.jpg',
+            descuento: 15
+        },
+        {
+            nombre: 'Nobles',
+            descripcion: ['Los Nobles', 'Soy Noble'],
+            precio: 7000,
+            imagen: '../../imagenes/categorías/vinos/nobles.jpg',
+            descuento: 0
+        },
+        {
+            nombre: 'Terrazas',
+            descripcion: ['Terrazas', 'Estoy arriba de tu edificio'],
+            precio: 3000,
+            imagen: '../../imagenes/categorías/vinos/terrazas.jpg',
+            descuento: 0
+        }
+    ],
+
+
+};
