218e38e106792379452d36dd0a3b279776d29e26
Cesar Lorenzon <lorenzon.cesar@hotmail.com>
2024-09-25 18:54:54 -0300

    Mejorada visibilidad del navbar y el footer en todo el sitio

--- Bootcamp_Full-Stack/semana-6/proyecto/Componentes/NavBarS.js
+++ Bootcamp_Full-Stack/semana-6/proyecto/Componentes/NavBarS.js
@@ -12,41 +12,38 @@
 //             .then(template => {
 //                 elemento.innerHTML = template;
 //                 console.log(template);
-                
+
 //             })
 //             .catch(error => console.error('Error al cargar el navbar:', error));
 //     }
 // }
 
-
 // const rutaNavBar = '../../../Componentes/NavBar.html';  // Ruta del navbar
 // const rutaFooter = '../../../Componentes/Footer.html'; // Ruta del footer
 
+// const rutaNavBar = '/semana-6/proyecto/Componentes/NavBar.html';
+// const rutaFooter = '/semana-6/proyecto/Componentes/Footer.html';
 
+// const rutaNavBar = '/Bootcamp_Full-Stack/semana-6/proyecto/Componentes/NavBar.html';
+// const rutaFooter = '/Bootcamp_Full-Stack/semana-6/proyecto/Componentes/Footer.html';
 
-// const rutaNavBar = '/semana-6/proyecto/Componentes/NavBar.html'; 
-// const rutaFooter = '/semana-6/proyecto/Componentes/Footer.html'; 
+const rutaNavBar = "../../Componentes/NavBar.html"; // Ruta del navbar
+const rutaFooter = "../../Componentes/Footer.html";
 
-// const rutaNavBar = '/Bootcamp_Full-Stack/semana-6/proyecto/Componentes/NavBar.html'; 
-// const rutaFooter = '/Bootcamp_Full-Stack/semana-6/proyecto/Componentes/Footer.html'; 
-
-const rutaNavBar = '../../Componentes/NavBar.html';  // Ruta del navbar
-const rutaFooter = '../../Componentes/Footer.html';
-
-document.addEventListener('DOMContentLoaded', () => {
-    cargarComponente('navbar-reutilizable', rutaNavBar); // Cargar navbar
-    cargarComponente('footer-reutilizable', rutaFooter); // Cargar footer
+document.addEventListener("DOMContentLoaded", () => {
+    cargarComponente("navbar-reutilizable", rutaNavBar); // Cargar navbar
+    cargarComponente("footer-reutilizable", rutaFooter); // Cargar footer
 });
 
 function cargarComponente(elementId, ruta) {
     const elemento = document.getElementById(elementId);
     if (elemento) {
         fetch(ruta)
-            .then(response => response.text())
-            .then(template => {
-                elemento.innerHTML = template;
+            .then((response) => response.text())
+            .then((template) => {
+                elemento.outerHTML = template;
                 console.log(`Componente ${elementId} cargado`);
             })
-            .catch(error => console.error(`Error al cargar el componente ${elementId}:`, error));
+            .catch((error) => console.error(`Error al cargar el componente ${elementId}:`, error));
     }
 }
